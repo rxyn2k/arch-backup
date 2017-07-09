12bb0b2583bcbefc77ccf35f6024e84b5e4ea9bd
rxyn2k <raynn.exe@gmail.com>
2017-07-09 11:10:03 +0300

    Update lock.sh

--- lock.sh
+++ lock.sh
@@ -11,4 +11,4 @@ RES=1280x800
 
 ffmpeg -f xllgrap -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG
 i3lock -u -i $TMPBG
-
+rm /tmp/screen.png
